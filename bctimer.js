1458fcc1a99748830024aa6cdfe36f3f380089e1
Brett Tuttle <tuttle.brett@gmail.com>
2014-09-23 23:38:31 -0400

    Added a basic demo. Added basic progress feature, but there are issues switching between modes. Fixed odd formatting.

--- bctimer.js
+++ bctimer.js
@@ -1,118 +1,165 @@
 (function ($)
- {
-     $.fn.bctimer = function(options)
-     {
-         var opts = $.extend({}, $.fn.bctimer.defaults, options);
+{
+    $.fn.bctimer = function(options)
+    {
+        var opts = $.extend({}, $.fn.bctimer.defaults, options);
          
-         var $this = this;
-         $this.addClass(".bctimer")
-             .css("width", opts.size)
-             .css("height", opts.size)
-             .css("background-color", opts.colors[0]);
+        var $this = this;
+        var leftBlocker = null;
+        var rightBlocker = null;
+        var rotationDegrees = 0;
+        var backgroundIndex = 0;
          
-         var leftBlocker = $("<div>")
-             .width($this.width()/2)
-             .height($this.height())
-             .css("background", opts.colors[opts.colors.length-1]);
-         
-         var rightBlocker = leftBlocker.clone()
-             .addClass("bctimer-rightblocker")
-         
-         leftBlocker.addClass("bctimer-leftblocker");
-         
-         $this.append(leftBlocker).append(rightBlocker);
-         
-         var rotationDegrees = 0;
-         var backgroundIndex = 0;
-         
-         function rotate()
-         {
-             if(rotationDegrees >= 359)
-             {
-                 rotationDegrees = rotationDegrees % 360;
-                 leftBlocker.css("background", $this.css("background"));
-                 rightBlocker.css("background", $this.css("background"));
-                 backgroundIndex++;
-                 if(backgroundIndex >= opts.colors.length)
-                 {
-                     backgroundIndex = 0;
-                 }
-                 $this.css("background", opts.colors[backgroundIndex]);
-                 
-                 leftBlocker.css({
-                     '-webkit-transform' : 'rotate(0deg)',
-                     '-moz-transform'    : 'rotate(0deg)',
-                     '-ms-transform'     : 'rotate(0deg)',
-                     '-o-transform'      : 'rotate(0deg)',
-                     'transform'         : 'rotate(0deg)'
-                 });
-                 
-                 rightBlocker.css({
-                     '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
-                     '-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
-                     '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
-                     '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
-                     'transform'         : 'rotate(' + rotationDegrees + 'deg)'
-                 });
-             }
+        if(!$this.hasClass("bctimer"))
+        {
+            $this.addClass("bctimer")
+                .css("width", opts.size)
+                .css("height", opts.size)
+                .css("background-color", opts.colors[0]);
              
-             else if(rotationDegrees > 179)
-             {
-                 leftBlocker.css({
-                     '-webkit-transform' : 'rotate(' + (rotationDegrees - 180) + 'deg)',
-                     '-moz-transform'    : 'rotate(' + (rotationDegrees - 180) + 'deg)',
-                     '-ms-transform'     : 'rotate(' + (rotationDegrees - 180) + 'deg)',
-                     '-o-transform'      : 'rotate(' + (rotationDegrees - 180) + 'deg)',
-                     'transform'         : 'rotate(' + (rotationDegrees - 180) + 'deg)' 
-                 });
-                 
-                 rightBlocker.css({
-                     '-webkit-transform' : 'rotate(0deg)',
-                     '-moz-transform'    : 'rotate(0deg)',
-                     '-ms-transform'     : 'rotate(0deg)',
-                     '-o-transform'      : 'rotate(0deg)',
-                     'transform'         : 'rotate(0deg)'
-                 });
-                 rightBlocker.css("background", $this.css("background"));
-             }
-             else
-             {
-                 leftBlocker.css({
-                     '-webkit-transform' : 'rotate(0deg)',
-                     '-moz-transform'    : 'rotate(0deg)',
-                     '-ms-transform'     : 'rotate(0deg)',
-                     '-o-transform'      : 'rotate(0deg)',
-                     'transform'         : 'rotate(0deg)'
-                 });
-                 rightBlocker.css({
-                     '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
-                     '-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
-                     '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
-                     '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
-                     'transform'         : 'rotate(' + rotationDegrees + 'deg)'
-                 });
-             }
-         }
-         
-         if(opts.mode === "spinner")
-         {
-             var step = 360 / (opts.cycleLength / opts.updateInterval);
-             function spin() {
-                 console.log("spin " + rotationDegrees);
-                 rotationDegrees += step;
-                 rotate();
-                 setTimeout(spin, opts.updateInterval);
-             }
+            $this.css({
+                        "-webkit-transform" : "rotate(0deg)",
+                        "-moz-transform"    : "rotate(0deg)",
+                        "-ms-transform"     : "rotate(0deg)",
+                        "-o-transform"      : "rotate(0deg)",
+                        "transform"         : "rotate(0deg)"
+            });
              
-             spin();
-         }
-     }
-     
-     $.fn.bctimer.defaults = {
-         mode: "spinner", // countdown | progress
-         size: "100px",
-         colors: ["black", "white"],
-         updateInterval: 25, //milliseconds between each update
-         cycleLength: 2500, //milliseconds for a complete rotation
-     }
- }(jQuery));
+            leftBlocker = $("<div>")
+                .width($this.width()/2)
+                .height($this.height())
+                .css("background-color", opts.colors[opts.colors.length-1]);
+             
+            rightBlocker = leftBlocker.clone()
+                .addClass("bctimer-rightblocker")
+             
+            leftBlocker.addClass("bctimer-leftblocker");
+             
+            $this.append(leftBlocker).append(rightBlocker);
+             
+            $this.data("colors", opts.colors);
+        }
+        else
+        {
+            $this.data("stopped", true);
+            leftBlocker = $this.find(".bctimer-leftblocker");
+            rightBlocker = $this.find(".bctimer-rightblocker");
+            opts.colors = $this.data("colors");
+        }
+         
+        function rotate()
+        {
+        console.log("rotate");
+            if(rotationDegrees >= 359)
+            {
+                rotationDegrees = rotationDegrees % 359;
+                leftBlocker.css("background-color", opts.colors[backgroundIndex]);
+                rightBlocker.css("background-color", opts.colors[backgroundIndex]);
+                backgroundIndex++;
+                if(backgroundIndex >= opts.colors.length)
+                {
+                    backgroundIndex = 0;
+                }
+                $this.css("background-color", opts.colors[backgroundIndex]);
+                 
+                leftBlocker.css({
+                    "-webkit-transform" : "rotate(0deg)",
+                    "-moz-transform"    : "rotate(0deg)",
+                    "-ms-transform"     : "rotate(0deg)",
+                    "-o-transform"      : "rotate(0deg)",
+                    "transform"         : "rotate(0deg)"
+                });
+                
+                rightBlocker.css({
+                    "-webkit-transform" : "rotate(" + rotationDegrees + "deg)",
+                    "-moz-transform"    : "rotate(" + rotationDegrees + "deg)",
+                    "-ms-transform"     : "rotate(" + rotationDegrees + "deg)",
+                    "-o-transform"      : "rotate(" + rotationDegrees + "deg)",
+                    "transform"         : "rotate(" + rotationDegrees + "deg)"
+                });
+            }
+             
+            else if(rotationDegrees > 179)
+            {
+                leftBlocker.css({
+                    "-webkit-transform" : "rotate(" + (rotationDegrees - 180) + "deg)",
+                    "-moz-transform"    : "rotate(" + (rotationDegrees - 180) + "deg)",
+                    "-ms-transform"     : "rotate(" + (rotationDegrees - 180) + "deg)",
+                    "-o-transform"      : "rotate(" + (rotationDegrees - 180) + "deg)",
+                    "transform"         : "rotate(" + (rotationDegrees - 180) + "deg)" 
+                });
+                
+                rightBlocker.css({
+                    "-webkit-transform" : "rotate(0deg)",
+                    "-moz-transform"    : "rotate(0deg)",
+                    "-ms-transform"     : "rotate(0deg)",
+                    "-o-transform"      : "rotate(0deg)",
+                    "transform"         : "rotate(0deg)"
+                });
+                rightBlocker.css("background-color", opts.colors[backgroundIndex]);
+            }
+            else
+            {
+                leftBlocker.css({
+                    "-webkit-transform" : "rotate(0deg)",
+                    "-moz-transform"    : "rotate(0deg)",
+                    "-ms-transform"     : "rotate(0deg)",
+                    "-o-transform"      : "rotate(0deg)",
+                    "transform"         : "rotate(0deg)"
+                });
+                rightBlocker.css({
+                    "-webkit-transform" : "rotate(" + rotationDegrees + "deg)",
+                    "-moz-transform"    : "rotate(" + rotationDegrees + "deg)",
+                    "-ms-transform"     : "rotate(" + rotationDegrees + "deg)",
+                    "-o-transform"      : "rotate(" + rotationDegrees + "deg)",
+                    "transform"         : "rotate(" + rotationDegrees + "deg)"
+                });
+                //console.log(backgroundIndex);
+                if(backgroundIndex == 0)
+                {
+                   rightBlocker.css("background-color", opts.colors[opts.colors.length-1]);
+                }
+                else
+                {
+                   rightBlocker.css("background-color", opts.colors[backgroundIndex - 1]);
+                }
+            }             
+        }
+         
+        if(opts.mode === "spinner")
+        {
+            $this.data("stopped", false);
+            var step = 360 / (opts.cycleLength / opts.updateInterval);
+            function spin() {
+                rotationDegrees += step;
+                rotate();
+                if(!$this.data("stopped"))
+                {
+                    setTimeout(spin, opts.updateInterval);
+                }
+            }
+            setTimeout(spin, opts.updateInterval);
+        }
+        else if(opts.mode === "progress")
+        {
+            if(typeof(opts.progress) !== "number" || opts.progress > 1 || opts.progress < 0)
+            {
+                console.error("Progress must be a value between 0 and 1 inclusive");
+                return;
+            }
+            rotationDegrees = 360 * opts.progress;
+            rotate();
+        }
+    }
+          
+    $.fn.bctimer.defaults = {
+        mode: "spinner", // countdown | progress
+        size: "100px",
+        colors: ["black", "white"],
+        updateInterval: 30, //milliseconds between each update
+        cycleLength: 5000, //milliseconds for a complete rotation
+        progress: 0, //between 0 and 1, only used for progress mode
+    }
+}(jQuery));
+
+$(".bctimer").bctimer({updateInterval: 100, cycleLength: 20000});
